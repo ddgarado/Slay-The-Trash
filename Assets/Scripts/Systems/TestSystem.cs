afddf132dc4530e2388198b39475715384277b43
Daniel Garado <122231506+ddgarado@users.noreply.github.com>
2025-05-23 10:42:48 +0800

    Card Architecture

--- Assets/Scripts/Systems/TestSystem.cs
+++ Assets/Scripts/Systems/TestSystem.cs
@@ -1,17 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestSystem : MonoBehaviour
 {
     [SerializeField] private HandView handView;
+    [SerializeField] private CardData cardData;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             // Vector2 touchPosition = Input.GetTouch(0).position;
-
-            CardView cardView = CardViewCreator.Instance.CreateCardView(transform.position, Quaternion.identity);
+            Card card = new(cardData);
+            CardView cardView = CardViewCreator.Instance.CreateCardView(card, transform.position, Quaternion.identity);
             StartCoroutine(handView.AddCard(cardView));
-        } 
+        }
     }
 }
